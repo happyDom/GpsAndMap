82328455e693a4de148338ff9ea4ae5391d3ae42
douyaoyuan <douyaoyuan@126.com>
2023-09-13 22:48:19 +0800

    添加 README.md 文档

--- src/GpsAndMap/MapModule.py
+++ src/GpsAndMap/MapModule.py
@@ -35,7 +35,7 @@ except ImportError as imErr:
             raise exp1
 
 try:
-    from selfPyTools.GpsModule import *
+    from GpsModule import *
 except ImportError as imErr:
     print('尝试导入 GpsModule 模块遇到异常:', imErr)
     raise imErr
